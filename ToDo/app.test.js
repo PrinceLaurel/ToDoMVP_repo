f5c825c31601b147ac2b443ecf16f5061262c7ae
PrinceLaurel <prince.laurel@edu.huddinge.se>
2023-10-05 16:07:44 +0200

    first commit

---
+++ ToDo/app.test.js
@@ -0,0 +1,27 @@
+const request = require('supertest');
+const app = require('./app');
+
+describe('Checking if the route "/" works', () => {
+    test('I am alive', done => {
+        request(app)
+        .get('/')
+        .expect(200)
+        .end(done)
+    });
+});
+describe('Can get an answer from /users', () => {
+    test('GET /users', done => {
+        request(app)
+        .get('/users')
+        .expect('respond with a resource')
+        .end(done)
+    });
+});
+describe('Can get an answer from /testing', () => {
+    test('GET /testing', done => {
+        request(app)
+        .get('/testing')
+        .expect(200)
+        .end(done)
+    });
+});
