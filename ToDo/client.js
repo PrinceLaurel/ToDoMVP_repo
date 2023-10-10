65bf93c5af68fe9f12a91436d5ad0daa2f4712b9
PrinceLaurel <prince.laurel@edu.huddinge.se>
2023-10-10 16:28:32 +0200

    arbetar med fetch()

--- ToDo/client.js
+++ ToDo/client.js
@@ -1,5 +1,14 @@
-async function logTasks() {
-    const response = await fetch("http://localhost:3000/testing");
-    const tasks = await response.json();
+/*const api_url = "http://localhost:3000/testing"
+
+async function logTasks(url) {
+    const response = await fetch(url, "http://localhost:3000/testing");
+    let tasks = await response.json();
     console.log(tasks);
 }
+*/
+let fetchRes = fetch("http://localhost:3000/testing");
+
+fetchRes.then(res =>
+    res.json()).then(d => {
+        console.log(d)
+    })
